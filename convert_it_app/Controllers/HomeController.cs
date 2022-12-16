f258112a766a6ec07c6fbbf1ae4aac2ec2cfaf68
Vira Murray <vira.avramenko@gmail.com>
2022-12-16 11:37:26 -0500

    added Length controller & view

--- convert_it_app/Controllers/HomeController.cs
+++ convert_it_app/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace convert_it_app.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ convert_it_app/Controllers/LengthController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace convert_it_app.Controllers
+{
+    public class LengthController : Controller
+    {
+        // 
+        // GET: /Length/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Length/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
+
